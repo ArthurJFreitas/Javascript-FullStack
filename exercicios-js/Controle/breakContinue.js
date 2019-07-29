deec69218ecfe3d221163c2212bba50a88caaafb
Arthur Freitas <43019622+ArthurJFreitas@users.noreply.github.com>
2019-07-29 17:00:45 -0300

    Update breakContinue.js

--- exercicios-js/Controle/breakContinue.js
+++ exercicios-js/Controle/breakContinue.js
@@ -1,4 +1,4 @@
-const nums = [1, 2, 3 ,4 ,5 ,6 ,7 , 8, 9]
+const nums = [1, 2, 3 ,4 ,5 ,6 ,7 , 8, 9, 10]
 
 for(let x in nums){
     if (x == 5){
@@ -13,4 +13,13 @@ for (y in nums){
     }
     console.log(`${y} = ${nums[y]}`)
 
+}
+
+externo: for(a in nums){
+    for(b in nums){
+        if (a == 2 && b == 3){
+            break externo
+        }
+        console.log(`Par ${a},${b}`)
+    }
 }
